40d8ad2625cdb16a7f1d4b1c39f2d44f513f0dc5
Santiago Risso <santiago.risso2@gmail.com>
2023-07-19 19:26:44 -0300

    registrar modelos

--- Aplicaciones/Modelos/admin.py
+++ Aplicaciones/Modelos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import usuario, articulo, comentario, categoria
 
 # Register your models here.
+
+admin.site.register(usuario)
+admin.site.register(articulo)
+admin.site.register(comentario)
+admin.site.register(categoria)
