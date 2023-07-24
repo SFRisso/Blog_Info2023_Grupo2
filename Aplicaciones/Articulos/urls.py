70d08a0bf157e0b402e3ab7ec9f217a1236a00f8
yarakauff <133162614+yarakauff@users.noreply.github.com>
2023-07-24 13:59:57 -0300

    Update urls.py

--- Aplicaciones/Articulos/urls.py
+++ Aplicaciones/Articulos/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
+
 from .views import * 
+
 
 apps_name='aplicaciones.articulos'
 
@@ -7,4 +11,6 @@ urlpatterns = [
     path("agregar_articulo/", AgregarArticulo.as_view(), name='agregar_articulo' ),
     path("modificar_articulo/",ModificarArticulos.as_view(),name="modificar_articulos"),
     path("eliminar_articulo/",EliminarArticulos.as_view(),name="eliminar_articulo"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
