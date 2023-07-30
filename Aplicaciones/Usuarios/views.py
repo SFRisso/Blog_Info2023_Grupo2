c5e54fc66e3e4b33ec7d321164fb4587e3deeb62
SFRisso <santiago.risso2@gmail.com>
2023-07-30 07:25:01 -0300

    Update views.py

--- Aplicaciones/Usuarios/views.py
+++ Aplicaciones/Usuarios/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render
-from .models import articulo
-from django.db.models import Q
-
 # Create your views here.
 
-def index(request):
-    queryset = request.GET.get("buscar")
-    articulos = articulo.objects.filter(estado = True)
-    if queryset:
-        articulos = articulo.objects.filter(
-            Q(titulo__icontains = queryset) |
-            Q(contenido__icontains = queryset) |
-            Q(localidad__icontains = queryset) |
-            Q(modalidad__icontains = queryset)
-        ).distinct()
-    return render(request,'index/index.html',{'articulos':articulos})
